0189e83e61bb512252a68cb4b7831f62031ce641
Min Ho CHO <chominho96@naver.com>
2022-11-04 17:47:20 +0900

    feat: add Spring project
    add Spring project
    dependency list:
     - Spring Web
     - MySQL Drivier
     - Lombok
     - Validation
     - Thymeleaf
     - Spring Data JPA
     - H2 Database

---
+++ src/main/java/j2b/nft_generator/NftGeneratorApplication.java
@@ -0,0 +1,13 @@
+package j2b.nft_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/j2b/nft_generator/NftGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package j2b.nft_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
