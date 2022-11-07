da22739d9348872a9eecd26c3d83167b7f5f7a31
Min Ho CHO <chominho96@naver.com>
2022-11-08 00:13:10 +0900

    feat(TestController): [back-end] add TestController
    Add TestController to test the connection to item.html

---
+++ src/main/java/j2b/nft_generator/test/TestController.java
@@ -0,0 +1,18 @@
+package j2b.nft_generator.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Test Controller입니다.
+ * 각 페이지 로의 접근을 테스트합니다.
+ */
+@Controller
+public class TestController {
+
+    @GetMapping("/item")
+    public String item(Model model) {
+        return "item";
+    }
+}
