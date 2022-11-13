0d1d2944d99325c7337af9ad4dc0ea99a4c6cd16
Min Ho CHO <chominho96@naver.com>
2022-11-13 21:52:13 +0900

    feat(member): add member entity
    - add member entity
    - add createMember function

---
+++ src/main/java/j2b/nft_generator/member/Member.java
@@ -0,0 +1,49 @@
+package j2b.nft_generator.member;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+/**
+ * 사용자 엔티티 클래스입니다.
+ */
+@Entity
+@Getter
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "mbr_id")
+    private Long id;
+
+    @Column(name = "mbr_name")
+    private String name;
+
+    @Column(name = "mbr_login_id")
+    private String loginId;
+
+    @Column(name = "mbr_pwd", columnDefinition = "TEXT")
+    private String password;
+
+    protected Member() { }
+
+    private Member(String name, String loginId, String password) {
+        this.name = name;
+        this.loginId = loginId;
+        this.password = password;
+    }
+
+    /**
+     * 사용자 엔티티를 생성하는 함수입니다.
+     * createMember 함수로만 사용자는 생성될 수 있습니다.
+     * @param name 사용자 이름
+     * @param loginId 사용자 로그인 ID
+     * @param password 사용자 비밀번호
+     * @return 생성된 사용자 entity
+     */
+    public static Member createMember(String name, String loginId, String password) {
+        return new Member(name, loginId, password);
+    }
+
+}
