9800f2c49e251b4b0e708eb4005df1a0168edba3
Min Ho CHO <chominho96@naver.com>
2022-11-21 20:59:08 +0900

    refactor(import): refactor import statement

--- src/main/java/j2b/nft_generator/member/service/MemberService.java
+++ src/main/java/j2b/nft_generator/member/service/MemberService.java
@@ -7,13 +7,11 @@ import j2b.nft_generator.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 /**
  * Member 관련 서비스 클래스입니다.
