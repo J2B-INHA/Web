b87a8b312283a7993c32359790511619221e0769
Min Ho CHO <chominho96@naver.com>
2022-11-23 19:31:52 +0900

    fix(add item): change effect class
    1. Modify thymeleaf syntax so that enum class can be used normally in select tag

--- src/main/java/j2b/nft_generator/imageconverter/constant/Effect.java
+++ src/main/java/j2b/nft_generator/imageconverter/constant/Effect.java
@@ -1,38 +1,21 @@
 package j2b.nft_generator.imageconverter.constant;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * 이미지 변환 이펙트에 해당하는 enum 클래스입니다.
  * @version 1.0.0
  * @author CHO Min Ho
  */
+@Getter
+@RequiredArgsConstructor
 public enum Effect {
-    CARTOON("cartoon"),
-    SKETCH("sketch"),
-    DETAIL("detail");
+    CARTOON("cartoon", "만화화"),
+    SKETCH("sketch", "흑백 스케치"),
+    DETAIL("detail", "디테일 향상");
 
-    @Getter
+    private final String key;
     private final String value;
 
-    Effect(String value) {
-        this.value = value;
-    }
-
-    @JsonCreator
-    public static Effect from(String value) {
-        for (Effect effect : Effect.values()) {
-            if (effect.getValue().equals(value)) {
-                return effect;
-            }
-        }
-        return null;
-    }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
 }
