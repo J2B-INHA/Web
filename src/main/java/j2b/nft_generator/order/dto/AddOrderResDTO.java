97a7943a1cad5a5204238a5527db97229b4ec8c0
Min Ho CHO <chominho96@naver.com>
2022-11-24 19:07:31 +0900

    feat(order): add order service
    1. Added order service

---
+++ src/main/java/j2b/nft_generator/order/dto/AddOrderResDTO.java
@@ -0,0 +1,17 @@
+package j2b.nft_generator.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Order 엔티티 생성 관련 response DTO입니다.
+ * @version 1.0.0
+ * @author CHO Min Ho
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddOrderResDTO {
+    private Long orderId;
+}
---
+++ src/main/java/j2b/nft_generator/order/service/OrderService.java
@@ -0,0 +1,48 @@
+package j2b.nft_generator.order.service;
+
+import j2b.nft_generator.member.entity.Member;
+import j2b.nft_generator.member.service.MemberService;
+import j2b.nft_generator.nft.entity.Nft;
+import j2b.nft_generator.nft.repository.NftRepository;
+import j2b.nft_generator.order.dto.AddOrderResDTO;
+import j2b.nft_generator.order.entity.Order;
+import j2b.nft_generator.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+/**
+ * Order 엔티티 관련 서비스 클래스입니다.
+ * @version 1.0.0
+ * @author CHO Min Ho
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final NftRepository nftRepository;
+    private final MemberService memberService;
+
+    /**
+     * 주문을 하는 메서드입니다.
+     * @param nftId 구매할 NFT의 ID
+     * @return 생성된 Order 엔티티의 ID
+     */
+    public AddOrderResDTO createOrder(Long nftId) {
+        Optional<Nft> findNft = nftRepository.findById(nftId);
+
+        if (findNft.isEmpty()) {
+            // TODO : 공통 예외 처리 로직 필요
+        }
+
+        Order order = orderRepository.save(Order.createOrder(memberService.getLoginMember(), findNft.get()));
+
+        return new AddOrderResDTO(order.getId());
+    }
+
+}
